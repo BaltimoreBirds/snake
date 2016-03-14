0c6352a67fabba26f1f7c54cd0a1d537f295239c
Michael Swanson <mikeswanson12@msn.com>
2016-03-13 21:16:51 -0400

    draws starting position

--- snake.js
+++ snake.js
@@ -1,18 +1,45 @@
 ;var Snake = (function(){
 
+  var directionX = 0,
+      directionY = 0,
+      boardHeight,
+      boardWidth,
+      playing = false,
+      headPositionX,
+      headPositionY;
+
+
+  var snake = function(){
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+
+
+    ctx.beginPath();
+    // ctx.arc(75,75,50,0,Math.PI*2,true); // Outer circle
+    ctx.moveTo(headPositionX-5,headPositionY);
+    ctx.lineTo(headPositionX, headPositionY);
+    // ctx.arc(75,75,35,0,Math.PI,false);  // Mouth (clockwise)
+    // ctx.moveTo(65,65);
+    // ctx.arc(60,65,5,0,Math.PI*2,true);  // Left eye
+    // ctx.moveTo(95,65);
+    // ctx.arc(90,65,5,0,Math.PI*2,true);  // Right eye
+    ctx.stroke();
+  };
+
+  var moveSnake = function(){
+    setTimeout(function(){
+
+    },10);
+  };
+
 	return {
-		draw: function(){
-      var canvas = document.getElementById("canvas");
-      if (canvas.getContext) {
-        var ctx = canvas.getContext("2d");
-
-        ctx.fillStyle = "rgb(200,0,0)";
-        ctx.fillRect (10, 10, 55, 50);
-
-        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-        ctx.fillRect (30, 30, 55, 50);
-      }
-		},
-	}
+    begin: function(width, height){
+      boardHeight = height;
+      boardWidth = width;
+      headPositionX = Math.floor(width/2);
+      headPositionY = Math.floor(height/2);
+      snake();
+    }
+	};
 
 })(document);
