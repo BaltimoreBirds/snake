fd17dab91fd5f573c7d3eef1a127e363b9aceb6e
Michael Swanson <mikeswanson12@msn.com>
2016-03-14 21:30:50 -0400

    defines singly linked list for snake data storage

--- snake.js
+++ snake.js
@@ -1,37 +1,82 @@
-;var Snake = (function(){
-
-  var directionX = 0,
+;var SnakeGame = (function(){
+		
+  var directionX = 1,
       directionY = 0,
       boardHeight,
       boardWidth,
       playing = false,
       headPositionX,
-      headPositionY;
+      headPositionY,
+      interval,
+      canvas,
+      ctx,
+      snake;      
 
+  function SinglyList(){
+  	this._length = 0;
+  	this.head = null;
+  }
+  function snakeNode(x,y){
+  	this.xPos = x;
+  	this.yPos = y;
+  	this.next = null;
+  }
+  SinglyList.prototype.add = function(x,y){
+  	var node = new snakeNode(x,y),
+  			currentNode = this.head;
 
-  var snake = function(){
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
+  	//Empty List
+  	if(!currentNode){
+  		this.head = node;
+  		this._length++;
+  		return node;
+  	}
+  	//Non-empty list
+  	while(currentNode.next){ 
+  		currentNode = currentNode.next;
+  	}
 
-
-    ctx.beginPath();
-    // ctx.arc(75,75,50,0,Math.PI*2,true); // Outer circle
-    ctx.moveTo(headPositionX-5,headPositionY);
-    ctx.lineTo(headPositionX, headPositionY);
-    // ctx.arc(75,75,35,0,Math.PI,false);  // Mouth (clockwise)
-    // ctx.moveTo(65,65);
-    // ctx.arc(60,65,5,0,Math.PI*2,true);  // Left eye
-    // ctx.moveTo(95,65);
-    // ctx.arc(90,65,5,0,Math.PI*2,true);  // Right eye
-    ctx.stroke();
-    setTimeout(function(){
-      moveSnake();
-    },10);
+  	currentNode.next = node;
+  	this._length++;
+  	return node;
+  };
+  SinglyList.prototype.traverse = function(callback){
+  	var currentNode = this.head;
+  		while(currentNode){
+  			callback(currentNode);
+  			currentNode = currentNode.next;
+  		}
   };
 
-  var moveSnake = function(){
-    
+  var slither = function(){
+  	var canvas = document.getElementById("gameBoard");	
+  	ctx = canvas.getContext("2d");
 
+  	snake = new SinglyList();
+  	snake.add(headPositionX, headPositionY);
+  	snake.add(headPositionX, headPositionY-5);
+    run(true);
+  };
+
+  function run(playing){	  
+   	if(playing){
+   		interval = setInterval(function(){
+	      printSnake();
+	    },1000);
+   	}else{
+   		clearInterval(interval);
+   	}   	
+  }
+
+  var printSnake = function(){
+  	console.log('Moving!');
+  	ctx.clearRect(0,0,boardWidth, boardHeight);
+    ctx.beginPath();
+    ctx.moveTo(snake.head.xPos+5, snake.head.yPos);
+    snake.traverse(function(node){
+    	ctx.lineTo(node.xPos, node.yPos);
+    	ctx.stroke();
+    });
   };
 
 	return {
@@ -40,7 +85,11 @@
       boardWidth = width;
       headPositionX = Math.floor(width/2);
       headPositionY = Math.floor(height/2);
-      snake();
+      slither();
+    },
+    end: function(){
+    	console.log('end it!');
+    	run(false);
     }
 	};
 
